4fb8faa41fd7aa2f865b9c122e8e8ba540b68aee
Anurag Singh Jadoun <anurag.singh254934@gmail.com>
2026-06-11 18:21:53 +0530

    sdf

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -53,8 +53,8 @@ class AdminController {
       });
       res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
+        secure: true,
+        sameSite: "none",
         maxAge: 3600000,
       });
       res.status(200).json({ message: "Admin logged in successfully", token });
